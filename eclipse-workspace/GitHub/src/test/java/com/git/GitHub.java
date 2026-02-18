bcbd392b7c502b026059b8bf00880a0c2b0d7d19
Anbusan06 <anbu.santh061@gmail.com>
2026-02-18 14:11:57 +0530

    Second Project

---
+++ eclipse-workspace/GitHub/src/test/java/com/git/GitHub.java
@@ -0,0 +1,17 @@
+package com.git;
+
+public class GitHub {
+
+	public void method01() {
+
+		System.out.println("Method01");
+	}
+	public void method02() {
+
+		System.out.println("Method02");
+	}
+	public void method03() {
+
+		System.out.println("Method03");
+	}
+}
